e04e8589b6a31f6062f3d89d25040b251a94e58a
S.H.YANG <yang482@gmail.com>
2023-07-21 09:10:20 +0900

    구현 완료된 틱택토

--- src/components/tic-tac-toe/board-component.js
+++ src/components/tic-tac-toe/board-component.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
+import _ from 'lodash';
+
 import { Square } from 'components/tic-tac-toe/square-component';
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    /*
+    loop 코드로 변경 되었기 때문에 key 추가
+    */
+    return (
+      <Square
+        key={i}
+        value={this.props.squares[i]}
+        onClick={() => { 
+            /*
+            원래는 없는 내용이나 lodash 를 활용해 onclick 핸들러 확인
+            */
+            if(!_.isFunction(this.props.onClick)) {
+              return;
+            }
+            return this.props.onClick(i);
+          }
+        }
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
+        {
+          /* 
+          lodash를 활용하여 중복 코드 제거
+          _.range(9) : 0 ~ 8 까지의 값을 가지는 길이 9의 배열 생성
+          _.chunk(list, 3) : list 를 3개씩 잘라서 순서대로 저장한 배열
+          JSX내에서 반복문을 쓸때는 .map을 활용하거나 function을 통해서 만들어 와야한다.
+          */
+          _.chunk(_.range(9), 3).map((subList, index) => 
+            <div className="board-row" key={index}>
+              {
+                subList.map(square => this.renderSquare(square))
+              }
+            </div>
+          )
+        }
+        {/*
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -28,6 +61,7 @@ class Board extends React.Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        */}
       </div>
     );
   }
--- src/components/tic-tac-toe/square-component.js
+++ src/components/tic-tac-toe/square-component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Square extends React.Component {
+class SquareClass extends React.Component {
   render() {
     return (
       <button className="square">
@@ -10,4 +10,28 @@ class Square extends React.Component {
   }
 }
 
-export { Square };
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+/******** export는 외부에서 해당 내용을 import 할수 있게 해준다. ********/ 
+
+/*
+export default 키워드는 import 시 이름을 마음대로 지정 가능하다.
+default는 한 js파일에서 한번만 지정 가능하다.
+ex ) 
+import Sq from 'assets/components/tic-tac-toe/square-component';
+*/
+export default Square;
+
+/*
+exprt {} 키워드는 import 시의 이름을 강제한다.
+as 키워드로 Alias 지정 가능
+ex ) 
+import { Square, SquareFunction, SquareClass } from 'assets/components/tic-tac-toe/square-component';
+*/
+export { Square, Square as SquareFunction, SquareClass };
